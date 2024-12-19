70051255f1bdddb47f5e7651d1164d332b8b0d95
Seladin Begu <beguseladin@gmail.com>
2024-12-19 22:36:21 +0100

    commit

--- Backend/Projekti Web Shërbime & Web API/Projekti1/obj/Debug/net8.0/Projekti1.AssemblyInfo.cs
+++ Backend/Projekti Web Shërbime & Web API/Projekti1/obj/Debug/net8.0/Projekti1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekti1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e6b7d912f8163f86daf915c534729e5b779ca27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211257715d6938b6476daf421c3ea37b8d892795")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekti1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekti1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
